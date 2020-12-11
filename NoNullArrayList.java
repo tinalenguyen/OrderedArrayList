ac406463dcdada825a5026fd66f22892035a6fef
tinalenguyen <>
2020-12-11 02:24:06 -0500

    overrided add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -13,4 +13,17 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       super(startingCapacity);
     }
 
+    public boolean add(T element){
+//overrides add; appends to end of the list
+      if (element==null) throw new IllegalArgumentException("The element is null");
+      super.add(element);
+      return true;
+    }
+
+    public void add(int index, T element){
+//overrides add; adds the element at a certain index
+      if (element==null) throw new IllegalArgumentException("The element is null");
+      super.add(index, element);
+    }
+
 }
