7edd5f0efa0d6e121ec7ee209b11216d7eab262f
tinalenguyen <>
2020-12-11 01:55:49 -0500

    added constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,6 +3,14 @@ public class NoNullArrayList<T> extends ArrayList<T>{
 //override set and both add methods
 //add two constructors
 
+    public NoNullArrayList(){
+//default
+      super();
 
+    }
+    public NoNullArrayList(int startingCapacity){
+//startingcapacity
+      super(startingCapacity);
+    }
 
 }
